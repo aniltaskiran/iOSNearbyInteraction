632eca6abe3052dca4da9547e7b7f6b09f088ec0
Anıl Taşkıran <anil.taskiran@trendyol.com>
2020-06-27 11:55:38 +0300

    Initial Commit

---
+++ Nearby Interaction/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nearby Interaction
+//
+//  Created by Anıl on 27.06.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
